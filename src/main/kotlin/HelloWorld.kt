a090228f75d65ad506be3e689fa08983839caefb
SAMYAK99 <singhsamyak199@gmail.com>
2021-06-19 02:13:57 +0530

    Added Hello World

---
+++ src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+    // Hello World!!!
+    println("Hello World")
+
+    // Basic Arithmetic Operations
+    val a = 5
+    val b = 10
+    val c = a+b
+    val d = a*b
+    println(c)
+    println(d)
+
+    // Displaying User name
+    var name : String = "Walker"
+    println("Hello World! My name is $name")
+
+}
