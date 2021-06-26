689433b7a2c243a95831027b858ea66c2ab49496
SAMYAK99 <singhsamyak199@gmail.com>
2021-06-26 12:46:57 +0530

    Type Casting and Type Conversion

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
 
     // Hello World!!!
     println("Hello World")
@@ -6,13 +6,14 @@ fun main(){
     // Basic Arithmetic Operations
     val a = 5
     val b = 10
-    val c = a+b
-    val d = a*b
+    val c = a + b
+    val d = a * b
     println(c)
     println(d)
 
     // Displaying User name
-    var name : String = "Walker"
+    var name: String = "Walker"
     println("Hello World! My name is $name")
+
 
 }
