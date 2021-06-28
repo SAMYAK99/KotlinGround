df8399b2bfb90d250d4502016fce3515faca1a28
SAMYAK99 <singhsamyak199@gmail.com>
2021-06-28 22:20:03 +0530

    Inheritance & Polymorphism

---
+++ src/main/kotlin/Class/Inheritance.kt
@@ -0,0 +1,46 @@
+package Class
+
+/*
+Inheritance - Inheritance is one in which a new class is created (derived class)
+ that inherits the features from the already existing class(Base class).
+
+Polymorphism - Object behaving differently for same class
+Open keyword is used for inheritance  and for overriding the class
+ */
+
+open class Car (val name :String){
+
+    init {
+        println("This is Main Car")
+    }
+
+    open var age = 1
+   open fun isMoving(){
+        println("Car is moving")
+    }
+}
+
+// Inheriting the Properties of class Car
+class Amaze(name : String , val Manufacturer : String , override var age : Int) : Car(name){
+
+    init {
+        println("This is Amaze")
+    }
+
+    override fun isMoving() {
+       println("Inside the amaze overrided")
+        // accessing properties and functions of parent class by super keyword
+        super.isMoving()
+    }
+
+
+}
+
+fun main(){
+
+    val car = Amaze("Amaze-1" ,"Honda" , 4)
+    car.isMoving()
+    println("The age of car is ${car.age}")
+
+
+}
